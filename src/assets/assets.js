731fe7d2421c259beedc746476a63387cdca69fd
Juniorkpabitey <juniorkpabitey123@gmail.com>
2025-01-06 02:55:33 +0000

    removed duplicate profile assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -16,9 +16,6 @@ import community_img2 from './community_img2.jpg'
 import community_img4 from './community_img4.jpg'
 import community_img5 from './community_img5.jpg'
 import community_img6 from './community_img6.jpg'
-import profile_img_1 from './profile_img_1.png'
-import profile_img_2 from './profile_img_2.png'
-import profile_img_3 from './profile_img_3.png'
 
 export const assets = {
     logo,
@@ -81,29 +78,4 @@ export const projectsData = [
     
   ];
 
-  export const testimonialsData = [
-    {
-        name: "Donald Jackman",
-        title: "Marketing Manager",
-        image: profile_img_1,
-        alt: "Portrait of Donald Jackman",
-        rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    },
-    {
-        name: "Richard Nelson",
-        title: "UI/UX Designer",
-        image: profile_img_2,
-        alt: "Portrait of Richard Nelson",
-        rating: 4,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    },
-    {
-        name: "James Washington",
-        title: "Co-Founder",
-        image: profile_img_3,
-        alt: "Portrait of James Washington",
-        rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    }
-];
+ 
